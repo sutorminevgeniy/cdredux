9b55031bbfd3b0231a8d37179e4173855180b404
Sutormin Evgeniy <Sutormin@competentum.ru>
2017-11-14 17:56:51 +0300

    vmesto redux

--- src/index.js
+++ src/index.js
@@ -1,31 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Store from './store';
+
+const initialState = { count: 0 };
+
+function updateState(state, action) {
+    switch (action.type) {
+        case 'INCREMENT': return { count: state.count + action.amount };
+        case 'DECREMENT': return { count: state.count - action.amount };
+        case 'RESET': return { count: 0 };
+        default: return state;
+    }
+}
+
+const incrementAction = { type: 'INCREMENT', amount: 1 };
+const decrementAction = { type: 'DECREMENT', amount: 1 };
+const resetAction = { type: 'RESET'};
+
+const store = new Store(updateState, initialState);
+
 class Counter extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { count: 0 };
-
         this.increment = this.increment.bind(this);
         this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    componentDidMount(){
+        store.subscribe(() => this.forceUpdate());
     }
 
     increment() {
-        this.setState({ count: this.state.count + 1 });
+        store.update(incrementAction)
     }
 
     decrement() {
-        this.setState({ count: this.state.count - 1 });
+        store.update(decrementAction) 
+    }
+
+    reset() {
+        store.update(resetAction) 
     }
 
     render() {
         return (
             <div className="counter">
-                <span className="count">{this.state.count}</span>
+                <span className="count">{store.state.count}</span>
 
                 <div className="buttons">
                     <button className="decrement" onClick={this.decrement}>-</button>
+                    <button className="reset " onClick={this.reset}>R</button>
                     <button className="increment" onClick={this.increment}>+</button>
                 </div>
             </div>
