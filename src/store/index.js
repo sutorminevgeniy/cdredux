36a2d3b30b5ce25503e519f26e7c297ad081d970
Sutormin Evgeniy <Sutormin@competentum.ru>
2017-12-13 15:45:56 +0300

    fetch new dispatch

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,21 @@ import { createStore } from 'redux';
 
 import reducer from '../reducers';
 
+function addPromiseThunkSupport(store) {
+    const dispatch = store.dispatch;
+    
+    return action => {
+        if (typeof action.then === 'function') {
+            return action.then(dispatch);
+        }
+        
+        return dispatch(action);
+    };
+}
+
 const store = createStore(reducer);
+
+store.dispatch = addPromiseThunkSupport(store);
+
 
 export default store;
