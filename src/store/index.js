11e0d88f1f7eee152bd7f7cef859ece38fbe4b3c
Sutormin Evgeniy <Sutormin@competentum.ru>
2017-12-14 15:31:40 +0300

    redux-promise redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
 
 import reducer from '../reducers';
 
-function addPromiseThunkSupport(store) {
-    const dispatch = store.dispatch;
-    
-    return action => {
-        if (typeof action.then === 'function') {
-            return action.then(dispatch);
-        } else if (typeof action === 'function') {
-            return action(dispatch);
-        }
-        
-        return dispatch(action);
-    };
-}
 
-const store = createStore(reducer);
-
-store.dispatch = addPromiseThunkSupport(store);
+const store = createStore(reducer, applyMiddleware(promise, thunk));
 
 export default store;
