c034223fab55850e2aa6b49ed5751e24e6a1c60f
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2018-03-11 20:36:41 +0300

    easier code

--- src/store/index.js
+++ src/store/index.js
@@ -2,42 +2,30 @@ import { createStore } from 'redux';
 
 import reducer from '../reducers';
 
-function addPromiseSupport(store) {
-  return dispatch => {
-    return action => {
-      if(typeof action.then === 'function') {
-        return action.then(dispatch);
-      }
+const addPromiseSupport = store => dispatch => action => {
+  if(typeof action.then === 'function') {
+    return action.then(dispatch);
+  }
 
-      return dispatch(action);
-    };
-  };
-}
+  return dispatch(action);
+};
 
-function addThunkSupport(store) {
-  return dispatch => {
-    return action => {
-      if(typeof action === 'function') {
-        return action(dispatch);
-      }
+const addThunkSupport = store => dispatch => action => {
+  if(typeof action === 'function') {
+    return action(dispatch);
+  }
 
-      return dispatch(action);
-    };
-  };
-}
+  return dispatch(action);
+};
 
-function addLogSupport(store) {
-  return dispatch => {
-    return action => {
-      console.log('Состояние до', store.getState());
-      console.log('Действие', action.type, action);
-  
-      dispatch(action);
-  
-      console.log('Состояние после', store.getState());
-    };
-  };
-}
+const addLogSupport = store => dispatch => action => {
+  console.log('Состояние до', store.getState());
+  console.log('Действие', action.type, action);
+
+  dispatch(action);
+
+  console.log('Состояние после', store.getState());
+};
 
 const store = createStore(reducer);
 const middlewares = [addLogSupport, addPromiseSupport, addThunkSupport]
