2b1d12927e8a6e71361e8470d7b17300e94077fb
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2018-03-11 16:21:18 +0300

    start 3 meddleware

--- src/store/index.js
+++ src/store/index.js
@@ -2,22 +2,47 @@ import { createStore } from 'redux';
 
 import reducer from '../reducers';
 
-function addPromiseThunkSupport(store) {
-    const dispatch = store.dispatch;
-    
-    return action => {
-        if (typeof action.then === 'function') {
-            return action.then(dispatch);
-        } else if (typeof action === 'function') {
-            return action(dispatch);
-        }
-        
-        return dispatch(action);
-    };
+function addPromiseSupport(store) {
+  const dispatch = store.dispatch;
+
+  return action => {
+    if(typeof action.then === 'function') {
+      return action.then(dispatch);
+    }
+
+    return dispatch(action);
+  };
+}
+
+function addThunkSupport(store) {
+  const dispatch = store.dispatch;
+
+  return action => {
+    if(typeof action === 'function') {
+      return action(dispatch);
+    }
+
+    return dispatch(action);
+  };
+}
+
+function addLogSupport(store) {
+  const dispatch = store.dispatch;
+
+  return action => {
+    console.log('Состояние до', store.getState());
+    console.log('Действие', action.type, action);
+
+    dispatch(action);
+
+    console.log('Состояние после', store.getState());
+  };
 }
 
 const store = createStore(reducer);
 
-store.dispatch = addPromiseThunkSupport(store);
+store.dispatch = addLogSupport(store);
+store.dispatch = addPromiseSupport(store);
+store.dispatch = addThunkSupport(store);
 
 export default store;
