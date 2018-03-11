eda9f45f9c1e4e6f60a72f711a636217bc8e8314
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2018-03-11 17:03:20 +0300

    easier carreing (like in redux)

--- src/store/index.js
+++ src/store/index.js
@@ -2,40 +2,46 @@ import { createStore } from 'redux';
 
 import reducer from '../reducers';
 
-function addPromiseSupport(store, dispatch) {
-  return action => {
-    if(typeof action.then === 'function') {
-      return action.then(dispatch);
-    }
+function addPromiseSupport(store) {
+  return dispatch => {
+    return action => {
+      if(typeof action.then === 'function') {
+        return action.then(dispatch);
+      }
 
-    return dispatch(action);
+      return dispatch(action);
+    };
   };
 }
 
-function addThunkSupport(store, dispatch) {
-  return action => {
-    if(typeof action === 'function') {
-      return action(dispatch);
-    }
+function addThunkSupport(store) {
+  return dispatch => {
+    return action => {
+      if(typeof action === 'function') {
+        return action(dispatch);
+      }
 
-    return dispatch(action);
+      return dispatch(action);
+    };
   };
 }
 
-function addLogSupport(store, dispatch) {
-  return action => {
-    console.log('Состояние до', store.getState());
-    console.log('Действие', action.type, action);
-
-    dispatch(action);
-
-    console.log('Состояние после', store.getState());
+function addLogSupport(store) {
+  return dispatch => {
+    return action => {
+      console.log('Состояние до', store.getState());
+      console.log('Действие', action.type, action);
+  
+      dispatch(action);
+  
+      console.log('Состояние после', store.getState());
+    };
   };
 }
 
 const store = createStore(reducer);
 const middlewares = [addLogSupport, addPromiseSupport, addThunkSupport]
 
-middlewares.forEach(middleware => store.dispatch = middleware(store, store.dispatch));
+middlewares.forEach(middleware => store.dispatch = middleware(store)(store.dispatch));
 
 export default store;
