944a46979d50a5cb70d5135ea5209fbff20c14c3
Sutormin Evgeniy <Sutormin@competentum.ru>
2018-03-12 20:09:49 +0300

    thunk, promise, logger from redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import reducer from '../reducers';
 
-const addPromiseSupport = store => next => action => {
-  if(typeof action.then === 'function') {
-    return action.then(next);
-  }
-
-  return next(action);
-};
-
-const addThunkSupport = store => next => action => {
-  if(typeof action === 'function') {
-    return action(next);
-  }
-
-  return next(action);
-};
-
-const addLogSupport = store => next => action => {
-  console.log('Состояние до', store.getState());
-  console.log('Действие', action.type, action);
-
-  let result = next(action);
-
-  console.log('Состояние после', store.getState());
-
-  return result;
-};
-
-const store = createStore(reducer, applyMiddleware(addLogSupport, addPromiseSupport, addThunkSupport));
-;
+const store = createStore(reducer, applyMiddleware(thunk, promise, logger));
 
 export default store;
